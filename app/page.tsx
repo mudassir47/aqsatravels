eea75a4fa1afc1dd172ff6d4bdd21b1eef6c937b
mudassir47 <mkteams472@gmail.com>
2024-11-05 23:10:21 -0800

    sdf

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,25 @@
+// app/page.tsx
+"use client";
+
 import Image from "next/image";
+import { useAuth } from "@/context/AuthContext"; // Adjust the path if necessary
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
+  const { user, loading } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!loading && !user) {
+      router.push("/unauthorized");
+    }
+  }, [user, loading, router]);
+
+  if (loading) {
+    return <div>Loading...</div>; // You can customize the loading state
+  }
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
